267733ef8d2257bfe63cb1ff31b299fb857298d5
Caiury Henrique <caiuryf@gmail.com>
2021-05-20 14:26:27 -0300

    usando-thymeleaf

--- src/main/java/com/algaworks/cobranca/controller/TituloController.java
+++ src/main/java/com/algaworks/cobranca/controller/TituloController.java
@@ -1,35 +1,45 @@
 package com.algaworks.cobranca.controller;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
+import com.algaworks.cobranca.model.StatusTitulo;
 import com.algaworks.cobranca.model.Titulo;
 import com.algaworks.cobranca.repository.TituloRepository;
 
 @Controller()
 @RequestMapping("/titulo")
 public class TituloController {
-	
-@Autowired	
-TituloRepository repository;	
 
-	
-	
+	@Autowired
+	TituloRepository repository;
+
 	@RequestMapping("/novo")
-	public String novo() {
+	public ModelAndView novo() {
+		ModelAndView mv = new ModelAndView("CadastroTitulo");
 
-		return "CadastroTitulo";
+		return mv;
 	}
-	
-	
-	
 
 	@RequestMapping("/salva")
-	public String salva(Titulo titulo) {
+	public ModelAndView salva(Titulo titulo) {
 
-  repository.save(titulo);
-	
-		return "CadastroTitulo";
+		repository.save(titulo);
+		ModelAndView mc = new ModelAndView("CadastroTitulo");
+		mc.addObject("mensagem", "Titulo Salvo com sucesso!");
+		return mc;
+	}
+
+	@ModelAttribute("todosStatusTitulo")
+	public List<StatusTitulo> todosStatusTitulo() {
+
+		return Arrays.asList(StatusTitulo.values());
+
 	}
 }
